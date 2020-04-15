85b81494e2f4bd62aefd83bfef428a5d369f178b
Dennis Prangle <dennis.prangle@gmail.com>
2020-04-15 15:25:38 +0100

    Minor extensions to Lorenz63 example for paper revision

--- Lorenz63 Example/plots.m
+++ Lorenz63 Example/plots.m
@@ -42,8 +42,8 @@ figure;
 subaxis('MT',0.03,'MB',0.08,'ML',0.08,'MR',0.05,'PL',0.01,'PR',0.01,'Pt',0.02,'PB',0.03,'spacing',0.05);
 theta0 = [log(10) log(28) log(8/3) log(2)/2 log(2)/2 log(2)/2]; %True parameter values
 parname = {'$\theta_1$', '$\theta_2$', '$\theta_3$', '$\sigma_1$', '$\sigma_2$', '$\sigma_3$'};
-bandwidths = [0.13, 0.1, 0.023, 0.05, 0.07, 0.1]; % Length of support (judged by eye) / 30
-ylims = [1, 1.5, 6, 2, 2, 1.5];
+bandwidths = [0.13, 0.1, 0.023, 0.05, 0.07, 0.1]; % Length of support, excluding EnKF (judged by eye) / 30
+ylims = [1.5, 1.5, 6, 3, 2, 2];
 
 for i = 1:6
     subaxis(3,2,i);
@@ -60,8 +60,8 @@ for i = 1:6
     [fx, x] = ksdensity(exp(theta_samp_EnKF_correlated(:,i)), 'Bandwidth', bandwidths(i));
     plot(x,fx,'-.g','LineWidth',2);
 
-    %%[fx, x] = ksdensity(exp(theta_samp_pEnKF(:,i)), 'Bandwidth', bandwidths(i));
-    %%plot(x,fx,':g','LineWidth',2);
+    [fx, x] = ksdensity(exp(theta_samp_pEnKF(:,i)), 'Bandwidth', bandwidths(i));
+    plot(x,fx,'c','LineWidth',0.5);
 
     v = exp(theta0(i));
     plot([v v], [0 ylims(i)], 'Color', 'black');
