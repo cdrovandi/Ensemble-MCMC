037e7f04f327af0cdf02cf465a8110f89595624d
Dennis Prangle <dennis.prangle@gmail.com>
2020-04-15 16:14:50 +0100

    Attempt at pMCMC code

---
+++ Lorenz96 Example/loglike_tuning_Lorenz96_pMCMC.m
@@ -0,0 +1,35 @@
+% Try to tune number of particles to get Var(log likelihood) roughly equal to 1.5
+
+load('data_Lorenz96.mat')
+
+sim_noise_scale = sqrt(10);
+dt = 0.01;
+steps_per_obs = 20;
+
+% True parameter values (not on log scale)
+thetaA = [1 1 8]; % Main parameters
+thetaB = 5; % Observation scale
+
+% Numbers of particles to try
+nreps = 30;
+nparticles_list = 500:500:10000;
+ntrials = length(nparticles_list);
+
+rng(1);
+
+var_ll_estimates_bpf = zeros(1, ntrials);
+ll_estimates = zeros(1, nreps);
+for i = 1:ntrials
+    i
+    for j = 1:nreps
+        ll_estimates(j) = BPF_Lorenz96(xinit, Y, n, T, nparticles_list(i), thetaA, thetaB, sim_noise_scale, dt, steps_per_obs);
+    end
+    var_ll_estimates_bpf(i) = var(ll_estimates);
+end
+
+figure;
+plot(nparticles_list, log(var_ll_estimates_bpf));
+hold on;
+xlabel('particles')
+ylabel('log variance of log likelihood')
+plot(xlim, [log(1.5) log(1.5)], 'Color', 'black');
---
+++ Lorenz96 Example/pMCMC_Lorenz96.m
@@ -0,0 +1,48 @@
+function [theta_samp] = pMCMC(iters,xinit,y,n,T,theta0,cov_rw,N,proposal_scale,sim_noise_scale,dt,steps_per_obs)
+%Parameters:
+%
+% iters - number of MCMC iterations
+% xinit - initial state variables
+% y - matrix of observations (T rows, n columns)
+% n - number of state variables
+% T - number of observations
+% theta0 - initial log parameter values (3 main parameters and observation noise scale)
+% cov_rw - MCMC proposal covariance matrix (unscaled)
+% N - number of EnKF particles
+% proposal_scale - scaling factor for MCMC proposal covariance matrix
+% sim_noise_scale - scale of noise added at each simulation step
+% dt - time step used in simulator
+% steps_per_obs - how many simulator time steps to perform between observations
+
+prior_rate = 0.1; % exponential prior for all parameters with mean 10
+theta_samp = zeros(iters,4);
+theta = theta0;
+acceptances = 0;
+
+logprior_curr = sum(theta - prior_rate*exp(theta));
+
+loglike = BPF_Lorenz96(xinit,y,n,T,N,exp(theta(1:3)),exp(theta(4)),sim_noise_scale,dt,steps_per_obs);
+
+for i = 1:iters
+    i
+    
+    theta_prop = mvnrnd(theta, proposal_scale^2*cov_rw);
+    logprior_prop = sum(theta_prop - prior_rate*exp(theta_prop));
+    
+loglike_prop = BPF_Lorenz96(xinit,y,n,T,N,exp(theta_prop(1:3)),exp(theta_prop(4)),sim_noise_scale,dt,steps_per_obs);
+    
+    log_mh = loglike_prop - loglike + logprior_prop - logprior_curr;
+    
+    if (rand < exp(log_mh))
+        fprintf('*** accepted ***\n');
+        acceptances = acceptances + 1;
+        theta = theta_prop;
+        logprior_curr = logprior_prop;
+        loglike = loglike_prop;
+    end
+    
+    theta_samp(i,:) = theta;
+    
+end
+
+acc_rate = acceptances / iters
